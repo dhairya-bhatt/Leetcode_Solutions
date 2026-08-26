1ac3b8c2b246f78f14c04096c115b0004e4a6ecd
Dhairya Bhatt <105587815+dhairya-bhatt@users.noreply.github.com>
2026-08-26 23:37:04 +0530

    3207-MAKE-THREE-STRINGS-EQUAL Stats: Time: 0 ms (100%), Space: 19.3 MB (27.62%) - LeetHub

--- 3207-make-three-strings-equal/3207-make-three-strings-equal.py
+++ 3207-make-three-strings-equal/3207-make-three-strings-equal.py
@@ -1,12 +1,10 @@
 class Solution:
     def findMinimumOperations(self, s1: str, s2: str, s3: str) -> int:
-        if s1[0] != s2[0] or s1[0] != s3[0] or s2[0] != s3[0]:
-            return -1
         count = 0
         for x in range(min(len(s1),len(s2),len(s3))):
             if s1[x] == s2[x]== s3[x]:
                 count+=1
             else:
                 break
-        return len(s1)+len(s2)+len(s3) - 3 * count
+        return len(s1)+len(s2)+len(s3) - 3 * count if count >0 else -1
         
